c2273bbcfcbad11c3da9e0e2f69c7eb3bab5c57d
ninggc <ninggc@foxmail.com>
2018-03-05 21:38:01 +0800

    improve contact list

--- APP SOURCE/app/src/main/java/com/ninggc/trade/activity/ease/ConversationListFragment.java
+++ APP SOURCE/app/src/main/java/com/ninggc/trade/activity/ease/ConversationListFragment.java
@@ -1,9 +1,12 @@
 package com.ninggc.trade.activity.ease;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.hyphenate.easeui.ui.EaseConversationListFragment;
+import com.ninggc.trade.R;
 
 /**
  * @author Ning
@@ -17,6 +20,14 @@ public class ConversationListFragment extends EaseConversationListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.e(TAG, "onActivityCreated: " + "会话列表");
+
+        //点击显示所有好友
+        getActivity().findViewById(R.id.iv_all_friends).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), ContactActivity.class));
+            }
+        });
     }
 
     @Override
